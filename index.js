3f617e17c5623ef3c1dc5516644ef7653b57fc50
Fernando Franco Valle <74745388+feh-franc0@users.noreply.github.com>
2023-04-01 20:09:09 -0300

    corrigindo o arquivo js
    a variável responsável pelo botão reset não estava sendo chamado corretamente. Corrige o código e agora está funcionando corretamente

---
+++ index.js
@@ -0,0 +1,92 @@
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milliseconds");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let interval;
+// let minutes = 0;
+// let seconds = 0;
+// let milliseconds = 0;
+// let isPaused = false;
+let sec = 1;
+let min = 1;
+
+function time() {
+    console.log("cliquei no TIME")
+    interval = setInterval(() => {
+    if (sec == 60) {
+        secondsEl.textContent = "00";
+        sec = 1;
+        if (min < 10) {
+        minutesEl.textContent = "0" + min++;
+        } else {
+        minutesEl.textContent = min++;
+        }
+    } else {
+        if (sec < 10) {
+        secondsEl.textContent = "0" + sec++;
+        } else {
+        secondsEl.textContent = sec++;
+        }
+    }
+    }, 1000);
+}
+
+function stoptime() {
+    console.log("cliquei no STOP")
+    clearInterval(interval);
+}
+
+
+startBtn.addEventListener("click", time);
+
+resetBtn.addEventListener("click", stoptime);
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milliseconds");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let interval;
+// let minutes = 0;
+// let seconds = 0;
+// let milliseconds = 0;
+// let isPaused = false;
+let sec = 1;
+let min = 1;
+
+function time() {
+    console.log("cliquei no TIME")
+    interval = setInterval(() => {
+    if (sec == 60) {
+        secondsEl.textContent = "00";
+        sec = 1;
+        if (min < 10) {
+        minutesEl.textContent = "0" + min++;
+        } else {
+        minutesEl.textContent = min++;
+        }
+    } else {
+        if (sec < 10) {
+        secondsEl.textContent = "0" + sec++;
+        } else {
+        secondsEl.textContent = sec++;
+        }
+    }
+    }, 1000);
+}
+
+function stoptime() {
+    console.log("cliquei no STOP")
+    clearInterval(interval);
+}
+
+
+startBtn.addEventListener("click", time);
+
+resetBtn.addEventListener("click", stoptime);
