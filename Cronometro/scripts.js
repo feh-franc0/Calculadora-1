4b362c43a5b7c25def7e900dbc25f5701be6490e
Fernando Franco Valle <74745388+feh-franc0@users.noreply.github.com>
2023-04-01 20:26:30 -0300

    corrigindo o botao reset

--- Cronometro/scripts.js
+++ Cronometro/scripts.js
@@ -7,40 +7,40 @@ const resumeBtn = document.querySelector("#resumeBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
 let interval;
-let minutes = 0;
-let seconds = 0;
-let milliseconds = 0;
-let isPaused = false;
-let sec = 50;
+// let minutes = 0;
+// let seconds = 0;
+// let milliseconds = 0;
+// let isPaused = false;
+let sec = 1;
 let min = 1;
 
 function time() {
+    console.log("cliquei no TIME")
     interval = setInterval(() => {
-        if (sec==60) {
-            secondsEl.textContent = "00"
-            sec = 1
-            if (min<10){
-                minutesEl.textContent = "0" + min++
-            }
-            else {
-                minutesEl.textContent = min++
-            }
+    if (sec == 60) {
+        secondsEl.textContent = "00";
+        sec = 1;
+        if (min < 10) {
+        minutesEl.textContent = "0" + min++;
         } else {
-            if (sec<10){
-                secondsEl.textContent = "0"+ sec++
-            }
-            else {
-                secondsEl.textContent = sec++
-            }
+        minutesEl.textContent = min++;
         }
+    } else {
+        if (sec < 10) {
+        secondsEl.textContent = "0" + sec++;
+        } else {
+        secondsEl.textContent = sec++;
+        }
+    }
+    }, 1000);
+}
 
-    },1000)
- }
+function stoptime() {
+    console.log("cliquei no STOP")
+    clearInterval(interval);
+}
 
- function stoptime() {
-    clearInterval(interval)
- }
 
-startBtn.addEventListener("click",time)
+startBtn.addEventListener("click", time);
 
-pauseBtn.addEventListener("click", stoptime)
+resetBtn.addEventListener("click", stoptime);
